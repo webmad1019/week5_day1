baeb79cceb86b28b6ddf7dac70fe610f58977b30
Germán Álvarez <46670724+GermanAlvarez-IH@users.noreply.github.com>
2019-11-18 13:50:54 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,20 @@
+require('dotenv').config()
+
+const express = require('express')
+const path = require('path')
+const app = express()
+
+require('./configs/mongoose.config')
+require('./configs/passport.config')(app)
+require('./configs/middlewares.config')(app)
+require('./configs/locals.config')(app)
+
+const app_name = require('./package.json').name;
+const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
+
+//Base URL
+app.use('/', require('./routes/index.routes'))
+app.use('/', require('./routes/auth.routes'))
+app.use('/', require('./routes/roles.routes'))
+
+module.exports = app
